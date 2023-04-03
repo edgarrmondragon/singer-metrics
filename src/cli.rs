f56d84031e44c691fc3bc6575477a8e70e9c2580
Edgar R. M <edgarrm358@gmail.com>
2023-04-02 20:50:11 -0600

    refactor: Refactor main CLI function (#3)

--- src/cli.rs
+++ src/cli.rs
@@ -2,23 +2,25 @@ use std::path::PathBuf;
 
 use clap::{Parser, Subcommand};
 
+use crate::metric::Precision;
+
 #[derive(Parser)]
 #[command(author, version)]
 pub struct Cli {
     #[command(subcommand)]
     pub command: Commands,
+
+    /// The input file to read from
+    #[arg(short, long, value_name = "FILE")]
+    pub input: Option<PathBuf>,
 }
 
 #[derive(Subcommand)]
 pub enum Commands {
     /// Convert Singer metrics to InfluxDB line protocol
     LineProtocol {
-        /// The input file to read from
-        #[arg(short, long, value_name = "FILE")]
-        input: Option<PathBuf>,
-
         /// The timestamp precision to use
-        #[arg(short, long, default_value = "ns")]
-        precision: String,
+        #[arg(short, long, value_name = "PRECISION", default_value = "ns")]
+        precision: Precision,
     },
 }
--- src/protocol_trait.rs
+++ src/protocol_trait.rs
@@ -1,3 +1,5 @@
+use std::io::{BufReader, BufWriter, Read, Write};
+
 use crate::metric::Measurement;
 
 pub trait ProtocolTrait {
@@ -11,4 +13,33 @@ pub trait ProtocolTrait {
     ///
     /// A string representation of the measurement
     fn dump(&self, measurement: &Measurement) -> String;
+
+    /// Convert input
+    ///
+    /// # Arguments
+    ///
+    /// * `reader` - The reader to read from
+    /// * `writer` - The writer to write to
+    fn convert(&self, reader: BufReader<impl Read>, mut writer: BufWriter<impl Write>) {
+        Measurement::read(reader)
+            .map(|measurement| self.dump(&measurement.unwrap()))
+            .for_each(|line| writeln!(&mut writer, "{}", line).unwrap());
+    }
+
+    /// Convert input from reader to stdout
+    ///
+    /// # Arguments
+    ///
+    /// * `reader` - The reader to read from
+    fn to_stdout(&self, reader: BufReader<impl Read>) {
+        let stdout = std::io::stdout();
+        let writer = BufWriter::new(stdout.lock());
+        self.convert(reader, writer)
+    }
+
+    /// Pipe from stdin to stdout
+    fn pipe(&self) {
+        let stdin = std::io::stdin();
+        self.to_stdout(BufReader::new(stdin.lock()));
+    }
 }
